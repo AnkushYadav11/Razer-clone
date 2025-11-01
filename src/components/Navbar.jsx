a22cbbe6428f2ca23c0608467dab244fca3e2350
AnkushYadav <yadavankush1811@gmail.com>
2025-11-01 16:57:51 +0530

    dropdown added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Navbar.css';
 import Icon from "../image/favicon.png";
 import Cart from "../image/cart_icon.svg";
 import Search from "../image/search.svg";
@@ -10,9 +11,39 @@ const Navbar = () => {
           <img src={Icon} className="size-12 mt-1" alt="Icon" />
           <ul className="flex gap-15 pt-4">
             
-            <li className="">Store
-            </li>
+            <li className="parent">Store
+            <div className="child">
+              <div>
+              <h1>Explore</h1>
+              <ul className="flex flex-col gap-5 overflow-y-scroll h-[30em]">
+                <li>Shop The Latest In Razer</li>
+                <li>Only at Razer</li>
+                <li>Laptops & Accessories</li>
+                <li>Mice</li>
+                <li>Mats</li>
+                <li>Keyboards</li>
+                <li>Controllers</li>
+                <li>Audio</li>
+                <li>Chairs</li>
+                <li>Content Creation</li>
+                <li>Gamer Room</li>
+                <li>Apparel & Gear</li>
+                <li>More</li>
+              </ul>
+              </div>
+              <div>
+                <h1>More About</h1>
+                <ul>
+                  <li>Help</li>
+                  <li>FAQ</li>
+                  <li>Returns and Refunds</li>
+                  <li>Shipping Guide</li>
+                  <li>Razer Advantage</li>
+                </ul>
+              </div>
+            </div>
 
+            </li>
             <li>
               PC
             </li>
